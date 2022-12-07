492d026e4f909d4cf244afdf79bc4dd56d426679
alfianfl <alvian.wadada@gmail.com>
2022-12-08 00:03:54 +0700

    first commit

---
+++ cmp-communication-01-starting-setup/src/App.vue
@@ -0,0 +1,160 @@
+<template>
+  <section>
+    <header>
+      <h1>My Friends</h1>
+    </header>
+    <ul>
+      <friend-contact
+        v-for="friend in friends"
+        :key="friend.id"
+        :name="friend.name"
+        :phoneNumber="friend.phoneNumber"
+        :email="friend.email"
+        :isFavorite="friend.isFavorite"
+        :id="friend.id"
+        @toggleFavorite="toggleFavoriteStatus"
+      >
+      </friend-contact>
+      <p>
+        <button class="btn--primary">Button Color</button>
+        <button class="btn--ghost-primary">Button Color</button>
+        <button class="btn--flat-primary">Button Color</button>
+      </p>
+    </ul>
+    <input
+      max="20"
+      type="number"
+      v-for="(item, i) in input"
+      :key="i"
+      @change="handler($event, i)"
+   
+    />
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phoneNumber: "0123 45678 90",
+          email: "manuel@localhost.com",
+          isFavorite:true
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phoneNumber: "0987 654421 21",
+          email: "julie@localhost.com",
+          isFavorite:false
+        },
+      ],
+      count:25,
+      input:[
+        {value:1},
+        {value:0},
+        {value:0},
+        {value:0}
+      ]
+    };
+  },
+  methods:{
+    toggleFavoriteStatus(id){
+      const friendById = this.friends.find(friend => friend.id === id);
+
+      friendById.isFavorite = !friendById.isFavorite;
+    },
+    handler(e, idx){
+   
+
+
+           if(this.count == 0){
+            e.target.value=0
+          } else{
+        if (e.target.value > this.count){
+          e.target.value  = this.count;
+           this.input[idx].value  = this.count;
+          this.count = 0
+          
+          } else{
+   this.input[idx].value = e.target.value
+            this.count -=this.input[idx].value
+          }
+          }
+
+
+      console.log(this.input)
+
+
+    },
+
+  },
+      mounted(){
+      console.log(this.count);
+    },
+    updated(){
+        console.log(this.count);
+    }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: 'Jost', sans-serif;
+}
+body {
+  margin: 0;
+}
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
---
+++ cmp-communication-01-starting-setup/src/components/FriendContact.vue
@@ -0,0 +1,60 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? '(Favorite)' : '' }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <button @click="toggleFavorite">{{ isFavorite ? 'Delete' : 'Add' }} Favorite</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ email }}
+      </li>
+    </ul>
+  </li>
+
+</template>
+
+<script>
+export default {
+  props:{
+    id:{
+      type: String,
+      required:true,
+    },
+    name:{
+      type: String,
+      required:true,
+    },
+    phoneNumber:{
+      type: String,
+      required:true,
+    },
+    email:{
+      type: String,
+      required:true,
+    },
+    isFavorite:{
+      type: Boolean,
+      required:false,
+      default:false
+    }
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite(){
+      this.$emit('toggleFavorite', this.id)
+    }
+  }
+};
+</script>
